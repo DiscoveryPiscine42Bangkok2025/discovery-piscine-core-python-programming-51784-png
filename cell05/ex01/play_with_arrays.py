e77b302366be48c80910875fec0de6817e5adf7b
51784-png <51784@cru.ac.th>
2026-04-24 18:02:49 +0700

    Add files via upload

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Original array
+original_array = [2, -8, 9, 42, 8, 67, -2, 56]
+
+# Create new array by adding 2 to each value
+new_array = []
+
+for num in original_array:
+    new_array.append(num + 3)
+
+# Display both arrays
+print("Original array:", original_array)
+print("New array:", new_array)
