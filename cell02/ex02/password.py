f22fc3b9db1a9db780ea11b95b92563392f635ad
51784-png <51784@cru.ac.th>
2026-04-24 14:24:34 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password = "ton666"
+
+user_input = input("Enter password: ")
+
+if user_input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
