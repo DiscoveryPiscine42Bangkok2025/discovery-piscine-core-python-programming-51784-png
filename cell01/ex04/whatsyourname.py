f72cbdffbf824d999e79fa96b7a0afbcacec4aef
51784-png <51784@cru.ac.th>
2026-04-24 13:13:11 +0700

    Add files via upload

---
+++ cell01/ex04/whatsyourname.py
@@ -0,0 +1,7 @@
+# Ask user for first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display both names
+print(first_name)
+print(last_name)
