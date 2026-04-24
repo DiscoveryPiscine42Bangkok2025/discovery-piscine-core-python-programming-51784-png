1cbb6a6838e47b38f21aa5c774bd565a90b8c098
51784-png <51784@cru.ac.th>
2026-04-24 12:59:29 +0700

    Add files via upload

---
+++ cell01/ex02/name.py
@@ -0,0 +1,9 @@
+# Define variables
+first_name = "Kittin"
+last_name = "Suksang"
+
+# Concatenate last name and first name
+whole_name = last_name + first_name
+
+# Display whole_name
+print(whole_name)
