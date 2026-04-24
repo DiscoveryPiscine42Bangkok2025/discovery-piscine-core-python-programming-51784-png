f9cf60dc0a3a792d54edb9227732b2bdff7fc1ab
51784-png <51784@cru.ac.th>
2026-04-24 16:28:36 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Ask for two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Display results
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Division:", num1 / num2)
+print("Multiplication:", num1 * num2)
